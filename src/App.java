767b2b656e5083b4405cf3d9948b080bdf71d53d
modengann <99417989+modengann@users.noreply.github.com>
2023-09-25 21:11:14 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,16 @@
+/**
+ *  Write a Java program that reads an unspecified number of integers from the user 
+ *  until the user enters -1. For each number, indicate whether it is odd or even. 
+ *  After the user enters -1, print the count of even and odd numbers entered.
+ */
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
